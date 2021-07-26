2bf65df8c51bb735a1e982e93933a1f491f358b2
LaughingLemon <laughing.programmer@outlook.com>
2021-07-26 12:37:27 +0100

    Abstract classes

--- hello-kotlin/src/main/kotlin/Person.kt
+++ hello-kotlin/src/main/kotlin/Person.kt
@@ -2,13 +2,17 @@ interface Greeter {
     fun hello(name: String?): String = "Hello there $name!"
 }
 
-class Person(val name: String, val age: Int): Greeter {
-    override fun hello(name: String?): String {
-        return super.hello(this.name)
-    }
+abstract class Person(val name: String, val age: Int): Greeter {
+    override fun hello(name: String?) = "Hello $name, this is ${this.name}"
+    abstract fun farewell(): String
+}
+
+class Customer(val id: Int, name: String, age: Int): Person(name, age) {
+    override fun farewell() = "Bye"
 }
 
 fun main(args: Array<String>) {
-    val person = Person("John", 34)
-    print(person.hello(null));
+    val person : Person = Customer(1,"John", 34)
+    println(person.hello("Mark"))
+    println(person.farewell())
 }
