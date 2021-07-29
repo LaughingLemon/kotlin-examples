c9c5c0f92c2ac50992f0fa8dd3794accce7d03ef
LaughingLemon <laughing.programmer@outlook.com>
2021-07-29 16:06:42 +0100

    Map, distinct, groupBy and filter to create a set of subscription emails

--- hello-kotlin/src/main/kotlin/Collections.kt
+++ hello-kotlin/src/main/kotlin/Collections.kt
@@ -1,11 +1,34 @@
-val numbers = (1..100).toList()
+data class Subscriber(val name: String, val email: String?, val subscriptions: List<String>)
+
+val subscribers = listOf(
+    Subscriber("John Jones", "jjones123@gmail.com", listOf("Linux Format", "3D World")),
+    Subscriber("J Jones", "jjones123@gmail.com", listOf("Linux Format")),
+    Subscriber("Arthur Grunthorpe", null, listOf("Architects Review", "3D World")),
+    Subscriber("Keith Harrop", "kharrop@outlook.com", listOf("Linux Format")),
+)
+
+fun createSubscriptionEmail(magazineTitle: String, subs: Subscriber): String {
+    return """
+        email: ${subs.email}
+        Dear ${subs.name},
+        Your copy of $magazineTitle will be sent shortly.
+        
+    """.trimIndent()
+}
 
 fun main(args: Array<String>) {
-    val mod7 = numbers.filter { num -> num % 7 == 0 }
-    println(mod7)
-
-    val tmp = numbers.asSequence()
-        .filter { num -> num % 3 == 0 }
-        .filterNot { num -> num % 6 == 0 }
-    tmp.forEach{ print( "$it, " ) }
+    val magazinesSubscribed = subscribers.map { it.subscriptions }
+        .fold(listOf<String>()) { list, item -> list.plus(item)}
+        .distinct()
+    val magazinesEmails =
+        magazinesSubscribed.groupBy( keySelector = { it },
+            valueTransform = { title ->
+                //unfortunately this gives a list of list
+                subscribers.filter { subs -> subs.subscriptions.contains(title) }
+                    .filter { subs -> subs.email != null } } )
+    for ((magazine, listOfSubs) in magazinesEmails) {
+        for (sub in listOfSubs[0].distinctBy { sub -> sub.email }) {
+            println(createSubscriptionEmail(magazine, sub))
+        }
+    }
 }
