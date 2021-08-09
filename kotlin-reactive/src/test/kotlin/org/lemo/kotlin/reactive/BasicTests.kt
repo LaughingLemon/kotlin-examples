6bba8b03308e6800f078c0ac464c91f24dfaf917
LaughingLemon <laughing.programmer@outlook.com>
2021-08-09 14:38:34 +0100

    Demonstrate element stream delays testing issues

--- kotlin-reactive/src/test/kotlin/org/lemo/kotlin/reactive/BasicTests.kt
+++ kotlin-reactive/src/test/kotlin/org/lemo/kotlin/reactive/BasicTests.kt
@@ -2,13 +2,15 @@ package org.lemo.kotlin.reactive
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import reactor.core.publisher.Flux
 import reactor.test.StepVerifier
+import java.time.Duration
 import java.util.function.Supplier
 
 class BasicTests {
-    val logger = LoggerFactory.getLogger(BasicTests::class.java)
+    val logger: Logger = LoggerFactory.getLogger(BasicTests::class.java)
 
     @Test
     fun `Range of ints to strings`() {
@@ -44,4 +46,27 @@ class BasicTests {
             .verify()
     }
 
+    @Test
+    fun `Use step verifier on Delayed Int Range merge`() {
+        val evenNums = Flux.range(0, 10).filter { x -> x % 2 == 0 }
+        val oddNums = Flux.range(0, 10).filter { x -> x % 2 > 0 }
+
+        val fluxOfInts = Flux.merge(
+            evenNums.delayElements(Duration.ofMillis(500L)),
+            oddNums.delayElements(Duration.ofMillis(300L))
+        )
+
+        StepVerifier.create(fluxOfInts)
+            .expectNext(1)
+            .expectNext(0)
+            .expectNext(3, 5)
+            .expectNext(2)
+            .expectNext(7)
+            .expectNext(4)
+            .expectNext(9)
+            .expectNext(6, 8)
+            .expectComplete()
+            .verify()
+
+    }
 }
