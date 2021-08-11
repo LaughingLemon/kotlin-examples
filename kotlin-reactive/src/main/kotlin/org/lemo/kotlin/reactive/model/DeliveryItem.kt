701e915a33232acc260feef07d355d7035b4af0a
LaughingLemon <laughing.programmer@outlook.com>
2021-08-11 16:32:23 +0100

    Load data from SQL

--- kotlin-reactive/src/main/kotlin/org/lemo/kotlin/reactive/model/DeliveryItem.kt
+++ kotlin-reactive/src/main/kotlin/org/lemo/kotlin/reactive/model/DeliveryItem.kt
@@ -1,15 +1,14 @@
 package org.lemo.kotlin.reactive.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
+@Table(name = "DELIVERY_ITEM")
 class DeliveryItem(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long? = null,
+    @Column(name = "ITEM_NAME")
     val itemName: String,
     val quantity: Long
 )
--- kotlin-reactive/src/main/kotlin/org/lemo/kotlin/reactive/routing/DeliveryRouterConfig.kt
+++ kotlin-reactive/src/main/kotlin/org/lemo/kotlin/reactive/routing/DeliveryRouterConfig.kt
@@ -5,11 +5,9 @@ import org.lemo.kotlin.reactive.model.DeliveryServiceRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Flux
-import reactor.core.publisher.Mono
 
 @Configuration
 open class DeliveryRouterConfig(@Autowired val repository: DeliveryServiceRepository) {
