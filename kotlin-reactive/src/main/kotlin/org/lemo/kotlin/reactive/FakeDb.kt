e4941c97d3d038a2f95b4137146642685d0fe254
LaughingLemon <laughing.programmer@outlook.com>
2021-08-13 17:27:20 +0100

    Using filters, zip and maps

--- kotlin-reactive/src/main/kotlin/org/lemo/kotlin/reactive/FakeDb.kt
+++ kotlin-reactive/src/main/kotlin/org/lemo/kotlin/reactive/FakeDb.kt
@@ -1,32 +1,56 @@
 package org.lemo.kotlin.reactive
 
-import io.reactivex.Observable
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import io.reactivex.Maybe
+import io.reactivex.Single
+import io.reactivex.rxkotlin.toFlowable
 
-class FakeDb {
+class FakeDb: Db {
+    private val userMap = mutableMapOf(
+        1 to User(1, "Andrew"),
+        2 to User(2, "Alison"),
+        3 to User(3, "Justine")
+    )
 
-    fun getUsers() : Observable<Map.Entry<Int, String>> {
-        return Observable.fromIterable(mapOf(
-            1 to "Andrew",
-            2 to "Alison",
-            3 to "Justine"
-        ).entries)
-    }
+    private val pointsMap = mutableMapOf(1 to 20, 2 to 86, 3 to 17)
 
-    fun getPointsPerUser(userId: Int) : Observable<Int> {
-        val pointsMap = mapOf(1 to 20, 2 to 86, 3 to 17)
-        return Observable.just(pointsMap[userId])
-    }
+    override fun getAllUsers(): Flowable<User> = userMap.values.toFlowable()
+
+    override fun getUserById(id: Int): Maybe<User> =
+        getAllUsers().filter { it.id == id }.firstElement()
+
+    override fun getPointsForUserId(id: Int): Single<Int> =
+        if (pointsMap.containsKey(id))
+            Single.just(pointsMap[id])
+        else
+            Single.error { UserNotFound("Record $id is not found") }
+
+    override fun addUser(user: User): Completable =
+        Completable.fromAction { userMap.put(user.id, user) }
 
 }
+
+interface Db {
+    fun getAllUsers(): Flowable<User>
+    fun getUserById(id: Int): Maybe<User>
+    fun getPointsForUserId(id: Int): Single<Int>
+    fun addUser(user: User): Completable
+}
+
+data class User(val id: Int, val name: String)
+class UserNotFound(message: String?): Exception(message)
 
 fun main(args: Array<String>) {
     val db = FakeDb()
 
-    db.getUsers().map { user -> user.value }.subscribe { println(it) }
-    db.getUsers().flatMap { user ->
-        db.getPointsPerUser(user.key)
-            .zipWith(Observable.just(user.value), {
-                points, username -> "$username has $points"
-            })
-    }.subscribe { println(it) }
+    db.getAllUsers()
+        .flatMapMaybe { user -> db.getUserById(user.id) }
+        .subscribe { println(it) }
+
+    db.getAllUsers()
+        .flatMapSingle { Single.just(it).zipWith(db.getPointsForUserId(it.id), {
+            user, points -> "${user.name} has $points"
+        } ) }
+        .subscribe { println(it) }
 }
